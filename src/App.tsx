8fcdf6f77f8206196aaca714ed802593334e98d9
sunjing <sunjing@mzhcorp.com>
2022-12-05 19:44:49 +0800

    feat: MouseTracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Hello from './components/Hello';
 import LikeButton from './components/LikeButton';
+import MouseTracker from './components/MouseTracker';
 
 const App: React.FC = () => {
     return (
         <>
             <Hello message="Hello World" />
             <LikeButton />
+            <MouseTracker />
         </>
     );
 }
